7105acd146ab64f5e0c6517f14ab2823ee6cf271
itsmaggiemay <mmh332@nyu.edu>
2014-11-08 21:26:43 -0500

    made changes to css, created show and index files for posts are working. Working on creating new.

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 #INDEX (main page & lists of posts)
-get '/posts' do #the location of the page
-	erb :'posts/index' #where to find the code
+get '/posts' do 
+	@posts = Post.all 
+	erb :'/posts/index' 
 end
+
+#SHOW (finding each post and showing by itself)
+get '/posts/:id' do
+	@post = Post.find(params[:id])
+	erb :'/posts/show'
+end
+
+#NEW
+ get '/posts/new' do
+ 	@user = User.all
+ 	erb :'/posts/new'
+ end
