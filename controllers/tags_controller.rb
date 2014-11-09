8dc79dae26dc7f9906ed95fe3b0036a332698646
itsmaggiemay <mmh332@nyu.edu>
2014-11-09 04:26:07 -0500

    finish tags

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -10,3 +10,19 @@ get '/tags/:id' do
 	@tag = Tag.find(params[:id])
 	erb :'/tags/show'
 end
+
+
+#ADD POST TO Tag
+
+put 'tags/:id/add_tag' do
+	tag = Tag.find(params[:id])
+	post = Post.find(params[:post_id])
+	tag.posts << post
+	redirect "/tags/#{tag.id}"
+end
+
+put '/tags/:id/remove_tag'
+	tag = Tag.find(params[:id])
+	tag.posts.destroy(params[:post_id])
+	redirect "tags/#{tag.id}"
+end
