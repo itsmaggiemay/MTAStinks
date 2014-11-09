9b4e39ab5b22df883db46216e1c7bf821294e853
itsmaggiemay <mmh332@nyu.edu>
2014-11-09 11:59:18 -0500

    code completed

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -4,6 +4,21 @@ get '/tags' do
 	erb :'/tags/index'
 end
 
+# NEW
+get('/tags/new') do
+  erb :'tags/new' 
+end
+
+# CREATE
+post('/tags') do
+  new_tag = Tag.new(params[:tag])
+  if new_tag.save
+    redirect("/tags/#{new_tag.id}") 
+  else
+    redirect("/tags/new")
+  end
+end
+
 #SHOW
 get '/tags/:id' do
 	@posts = Post.all
@@ -12,17 +27,45 @@ get '/tags/:id' do
 end
 
 
-#ADD POST TO Tag
-
-put 'tags/:id/add_tag' do
-	tag = Tag.find(params[:id])
-	post = Post.find(params[:post_id])
-	tag.posts << post
-	redirect "/tags/#{tag.id}"
+# EDIT
+get('/tags/:id/edit') do
+  @tag = Tag.find(params[:id])
+  erb :'/tags/edit' 
 end
 
-put '/tags/:id/remove_tag' do
-	tag = Tag.find(params[:id])
-	tag.posts.destroy(params[:post_id])
-	redirect "tags/#{tag.id}"
+# UPDATE
+put('/tags/:id') do
+  tag = Tag.find(params[:id])
+  if tag.update(params[:tag])
+    redirect("/tags/#{tag.id}")
+  else 
+    redirect("/tags/#{tag.id}/edit")
+  end
+end
+
+# DESTROY
+delete('/tags/:id') do
+  tag = Tag.find(params[:id]) 
+  if tag.destroy
+    redirect("/tags")
+  else
+    redirect("/tags/#{tag.id}")
+  end
+end
+
+
+# ADD POST TO TAG
+put('/tags/:id/add_post') do
+  tag = Tag.find(params[:id])
+  post = Post.find(params[:post_id])
+  tag.posts << post
+  redirect("/tags/#{tag.id}")
+end
+
+# REMOVE POST FROM TAG
+put('/tags/:id/remove_post') do
+  tag = Tag.find(params[:id])
+  post = Post.find(params[:post_id])
+  tag.posts.destroy(post)
+  redirect("/tags/#{tag.id}")
 end
